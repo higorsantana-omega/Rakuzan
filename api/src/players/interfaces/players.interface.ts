718603d9f183e3c9965e50d822ba285660f9b4df
higorsantana-omega <higor.santanahs17@gmail.com>
2023-03-15 19:56:55 -0300

    validation route

--- api/src/players/interfaces/players.interface.ts
+++ api/src/players/interfaces/players.interface.ts
@@ -1,10 +1,10 @@
-import { Document } from "mongoose"
+import { Document } from 'mongoose';
 
 export default interface Player extends Document {
-  readonly phone: string
-  readonly email: string
-  name: string
-  ranking: string
-  position: number
-  photo: string
+  readonly phone: string;
+  readonly email: string;
+  name: string;
+  ranking: string;
+  position: number;
+  photo: string;
 }
--- api/src/players/players.controller.ts
+++ api/src/players/players.controller.ts
@@ -1,35 +1,37 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import CreatePlayerDTO from './dtos/create-player.dto';
 import Player from './interfaces/players.interface';
 import { PlayersService } from './players.service';
 
 @Controller('players')
 export class PlayersController {
-  constructor (
-    private readonly playersService: PlayersService
-  ) {}
+  constructor(private readonly playersService: PlayersService) {}
 
   @Post()
-  async createOrUpdatePlayer(
-    @Body() createPlayerDTO: CreatePlayerDTO
-  ) {
-    await this.playersService.createOrUpdatePlayer(createPlayerDTO)
+  @UsePipes(ValidationPipe)
+  async createOrUpdatePlayer(@Body() createPlayerDTO: CreatePlayerDTO) {
+    await this.playersService.createOrUpdatePlayer(createPlayerDTO);
   }
 
   @Get()
-  async getPlayers (
-    @Query('email') email: string
-  ): Promise<Player[] | Player> {
+  async getPlayers(@Query('email') email: string): Promise<Player[] | Player> {
     if (email) {
-      return this.playersService.getPlayerByEmail(email)
+      return this.playersService.getPlayerByEmail(email);
     }
-    return this.playersService.getAllPlayers()
+    return this.playersService.getAllPlayers();
   }
 
   @Delete()
-  async deletePlayer (
-    @Query('email') email: string
-  ): Promise<void> {
-    this.playersService.deletePlayer(email)
+  async deletePlayer(@Query('email') email: string): Promise<void> {
+    this.playersService.deletePlayer(email);
   }
 }
