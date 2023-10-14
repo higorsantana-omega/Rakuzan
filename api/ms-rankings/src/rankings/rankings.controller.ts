2dce3eb3b1270e996ba32e366c661e4e6bcf2b72
higorsantana-omega <higor.santanahs17@gmail.com>
2023-10-14 00:43:37 -0300

    feat(ms-rankings): register ranking when realized challenge

--- api/ms-rankings/src/rankings/rankings.controller.ts
+++ api/ms-rankings/src/rankings/rankings.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { Match } from './interfaces/ranking.schema';
+import { RankingsService } from './rankings.service';
+
+const ackErrors = ['E1100'];
 
 @Controller('rankings')
-export class RankingsController {}
+export class RankingsController {
+  private logger = new Logger(RankingsController.name);
+
+  constructor(private rankingsService: RankingsService) {}
+
+  @EventPattern('process-match')
+  async processMatch(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      this.logger.log(`data: ${JSON.stringify(data)}`);
+      const matchId = data.matchId as string;
+      const match = data.match as Match;
+
+      await this.rankingsService.processMatch({ matchId, match });
+
+      await channel.ack(originalMessage);
+    } catch (error) {
+      this.logger.error(`error: ${JSON.stringify(error.message)}`);
+
+      const errors = ackErrors.filter((ack) => error.message.includes(ack));
+
+      if (errors) {
+        await channel.ack(originalMessage);
+      }
+    }
+  }
+}
--- api/ms-rankings/src/rankings/rankings.service.ts
+++ api/ms-rankings/src/rankings/rankings.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Match, Ranking } from './interfaces/ranking.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class RankingsService {}
+export class RankingsService {
+  private readonly logger = new Logger(RankingsService.name);
+
+  constructor(@InjectModel('Ranking') private rankingModel: Model<Ranking>) {}
+
+  async processMatch(processMatchDTO: { matchId: string; match: Match }) {
+    this.logger.log(`${JSON.stringify(processMatchDTO)}`);
+
+    const { match, matchId } = processMatchDTO;
+
+    const rankingsPromises = match.players.map((player) => {
+      const ranking = new this.rankingModel();
+
+      ranking.category = match.category;
+      ranking.challenge = match.challenge;
+      ranking.match = matchId;
+      ranking.player = player;
+
+      const isWinner = player === match.def;
+      const isLoser = player !== match.def;
+
+      if (isWinner) {
+        ranking.event = 'VICTORY';
+        ranking.points = 30;
+        ranking.operation = '+';
+      }
+
+      if (isLoser) {
+        ranking.event = 'DEFEAT';
+        ranking.points = 0;
+        ranking.operation = '-';
+      }
+
+      this.logger.log(`ranking: ${JSON.stringify(ranking)}`);
+
+      return ranking.save();
+    });
+
+    await Promise.all(rankingsPromises);
+  }
+}
